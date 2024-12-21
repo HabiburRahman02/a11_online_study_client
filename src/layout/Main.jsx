0cbe8619bea181d151837f04b186bfa9b2e20c8b
Habibur Rahman <habibur88454@gmail.com>
2024-12-21 19:26:11 +0600

    initial setup done

---
+++ src/layout/Main.jsx
@@ -0,0 +1,21 @@
+import { Outlet } from "react-router-dom";
+
+const Main = () => {
+    return (
+        <div>
+            <div>
+
+            </div>
+
+            <div>
+                <Outlet></Outlet>
+            </div>
+
+            <div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Main;
