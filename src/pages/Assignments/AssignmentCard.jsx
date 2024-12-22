ecc512c3cb5781ea752c2d6a322ec5ba7df2765e
Habibur Rahman <habibur88454@gmail.com>
2024-12-22 15:28:30 +0600

    Sweet alert added and delete mathod apply

--- src/pages/Assignments/AssignmentCard.jsx
+++ src/pages/Assignments/AssignmentCard.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 
 const AssignmentCard = () => {
     return (
--- src/pages/Assignments/Assignments.jsx
+++ src/pages/Assignments/Assignments.jsx
@@ -1,7 +1,9 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { FaEdit, FaEye } from 'react-icons/fa';
+import { FaCircle, FaEdit, FaEye } from 'react-icons/fa';
 import { RiDeleteBack2Fill } from "react-icons/ri";
+import { toast } from "react-toastify";
+import Swal from "sweetalert2";
 
 const Assignments = () => {
     const [assignments, setAssignments] = useState([]);
@@ -14,27 +16,56 @@ const Assignments = () => {
             })
     }, []);
 
+    const handleDelete = id => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axios.delete(`http://localhost:5000/assignments/${id}`)
+                    .then(data => {
+                        console.log(data.data)
+                        if (data.data.deletedCount) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Deleted assignment successfully",
+                                icon: "success",
+                                showConfirmButton: false,
+                                timer: 1500
+                            });
+                        }
+                    })
+                    .catch(error => toast.error(error.message))
+            }
+        });
+    }
+
     return (
         <div className="overflow-x-auto max-w-[1400px] mx-auto">
             <table className="table">
                 {/* head */}
                 <thead className="bg-gradient-to-r from-blue-100 via-purple-100 to-pink-100 text-black">
                     <tr>
-                        <th>No</th>
-                        <th>Thumbnail</th>
-                        <th>Title</th>
-                        <th>Marks</th>
-                        <th>Difficulty</th>
-                        <th>Date</th>
-                        <th>Actions</th>
+                        <th className="py-6"> <FaCircle className="text-green-400"></FaCircle></th>
+                        <th className="py-6">Thumbnail</th>
+                        <th className="py-6">Title</th>
+                        <th className="py-6">Marks</th>
+                        <th className="py-6">Difficulty</th>
+                        <th className="py-6">Date</th>
+                        <th className="py-6">Actions</th>
                     </tr>
                 </thead>
                 <tbody>
                     {
-                        assignments.map(assignment => <tr
+                        assignments.map((assignment, i) => <tr
                             key={assignment._id}
                             className="hover:bg-gray-100  transition duration-300 py-4">
-                            <th className="py-8">1</th>
+                            <th className="py-8">{i + 1}</th>
                             <td className="py-8">
                                 <div>
                                     <img
@@ -62,7 +93,9 @@ const Assignments = () => {
                                     </button>
 
                                     {/* Delete Button */}
-                                    <button className="text-xl text-red-700 hover:text-red-500 hover:scale-110 transform transition duration-300 ease-in-out p-2 rounded-full hover:bg-red-100">
+                                    <button
+                                        onClick={() => handleDelete(assignment._id)}
+                                        className="text-xl text-red-700 hover:text-red-500 hover:scale-110 transform transition duration-300 ease-in-out p-2 rounded-full hover:bg-red-100">
                                         <RiDeleteBack2Fill />
                                     </button>
 
